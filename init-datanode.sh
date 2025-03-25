36d0c1282845e222deeeb2d3eda55483f1e6b0d7
Med-Anis-Oueslati <mohamedanis.oueslati@ensi-uma.tn>
2025-03-25 11:11:52 +0100

    fixing hadoop bugs

--- init-datanode.sh
+++ init-datanode.sh
@@ -2,4 +2,7 @@
 rm -rf /opt/hadoop/data/dataNode/*
 chown -R hadoop:hadoop /opt/hadoop/data/dataNode
 chmod 755 /opt/hadoop/data/dataNode
-hdfs datanode
+$HADOOP_HOME/bin/hdfs --daemon start datanode
+sleep 5
+HOSTNAME=$(hostname)
+tail -f /opt/hadoop/logs/hadoop-root-datanode-${HOSTNAME}.log || sleep infinity
--- start-hdfs.sh
+++ start-hdfs.sh
@@ -2,4 +2,6 @@
 if [ ! -d /opt/hadoop/data/nameNode/current ]; then
   $HADOOP_HOME/bin/hdfs namenode -format -force
 fi
-$HADOOP_HOME/bin/hdfs namenode
+$HADOOP_HOME/bin/hdfs --daemon start namenode
+sleep 5
+tail -f /opt/hadoop/logs/hadoop-root-namenode-*.log || sleep infinity
